0b101cd7cbe40e489f2d88e0a5faf588cda94c89
Jerry <yanjiarui1997@gmail.com>
2023-09-03 22:03:06 -0400

    IBL boilerplate

--- Tools/GenIBL/main.cpp
+++ Tools/GenIBL/main.cpp
@@ -1,4 +1,3 @@
-// #define VMA_IMPLEMENTATION
 #include "vk_mem_alloc.h"
 #include "args.hxx"
 
@@ -6,12 +5,62 @@
 #include "../../SharedLibrary/Utils/VulkanDbgUtils.h"
 
 #include <vulkan/vulkan.h>
+#include <Windows.h>
 
 #include "renderdoc_app.h"
 
-int main()
+int main(
+    int argc,
+    char** argv)
 {
+    args::ArgumentParser parser("This tool takes a cubemap as input and output its image based lighting data.",
+        "E.g. GenIBL.exe --srcPath ./img.hdr --dstPath ./iblOutput");
+    args::HelpFlag help(parser, "help", "Display this help menu", { 'h', "help" });
+    args::CompletionFlag completion(parser, { "complete" });
+
+    args::ValueFlag<std::string> inputPath(parser, "", "The input cubemap image.", { 'i', "srcPath" });
+    args::ValueFlag<std::string> outputPath(parser, "", "The output image based lighting data output folder.", { 'i', "dstPath" });
+
+    try
+    {
+        parser.ParseCLI(argc, argv);
+    }
+    catch (const args::Completion& e)
+    {
+        std::cout << e.what();
+        return 0;
+    }
+    catch (const args::Help&)
+    {
+        std::cout << parser;
+        return 0;
+    }
+    catch (const args::ParseError& e)
+    {
+        std::cerr << e.what() << std::endl;
+        std::cerr << parser;
+        return 1;
+    }
+
+    // RenderDoc debug starts
+    RENDERDOC_API_1_6_0* rdoc_api = NULL;
+    if (HMODULE mod = GetModuleHandleA("renderdoc.dll"))
+    {
+        pRENDERDOC_GetAPI RENDERDOC_GetAPI = (pRENDERDOC_GetAPI)GetProcAddress(mod, "RENDERDOC_GetAPI");
+        int ret = RENDERDOC_GetAPI(eRENDERDOC_API_Version_1_6_0, (void**)&rdoc_api);
+        assert(ret == 1);
+    }
+
+    if (rdoc_api)
+    {
+        rdoc_api->StartFrameCapture(NULL, NULL);
+    }
+
     GenIBL app;
     app.AppInit();
 
+    if (rdoc_api)
+    {
+        rdoc_api->EndFrameCapture(NULL, NULL);
+    }
 }
