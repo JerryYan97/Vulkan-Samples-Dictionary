5c4341f17dfdaed370c430bf09e0d3b8aa7b4a8a
Jerry <yanjiarui1997@gmail.com>
2024-01-02 21:44:54 -0500

    Put Gamma Correction Back

--- Samples/3-05_PBRDeferredRender/DebugSample/hlsl/geo_frag.hlsl
+++ Samples/3-05_PBRDeferredRender/DebugSample/hlsl/geo_frag.hlsl
@@ -94,8 +94,8 @@ float4 main(
     float3 color = Lo;
 	
     // Gamma Correction
-    // color = color / (color + float3(1.0, 1.0, 1.0));
-    // color = pow(color, float3(1.0/2.2, 1.0/2.2, 1.0/2.2));  
+    color = color / (color + float3(1.0, 1.0, 1.0));
+    color = pow(color, float3(1.0/2.2, 1.0/2.2, 1.0/2.2));  
 
 	return float4(color, 1.0);
 }
